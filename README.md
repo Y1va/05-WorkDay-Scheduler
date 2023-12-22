16f7233d7aa16b47105f7c410a2aa96402aaf4de
ZeuS <ponzeadrian@gmail.com>
2023-12-22 17:16:10 +1100

    Pushing final touches

---
+++ README.md
@@ -0,0 +1,29 @@
+# Challenge 05 - Work Day Scheduler
+
+
+## Description
+
+For this project my class and I were tasked with creating a calendar application that allows a user to save events for each hour of a typical working day.
+
+This app will run in the browser and feature dynamically udpated HTML and CSS powered by jQuery. We also had to utilise the Day.js library in order to work with the date and time. 
+
+
+## Usage
+
+You can view the page through this link....
+
+
+### Built With
+- HTML
+- CSS
+- JavaScript
+- Bootstrap
+- Day.js
+- JQuery
+
+
+### License
+
+This project is licensed under the [MIT License](./LICENSE).
+
+
--- assets/js/script.js
+++ assets/js/script.js
@@ -2,14 +2,24 @@
 // the code isn't run until the browser has finished rendering all the elements
 // in the html.
 
+  // Code to display the current date in the header of the page.
   var todaydate = dayjs();
   console.log(todaydate.format("dddd, MMMM D"));
   $("#currentDay").text(todaydate.format("dddd, MMMM D"));
-
-
-$(function () {
-  // TODO: Add a listener for click events on the save button. This code should
   
+
+$(function trackTask() {
+  // TODO: Add a listener for click events on the save button. 
+  $(".saveBtn").on('click', function () {
+  var valueOfText = $(this).siblings('.description').val();
+
+
+  })
+  // setting the current hour variable using dayjs
+  var currentHour = dayjs().hour();
+
+
+  // This code should
   // use the id in the containing time-block as a key to save the user input in
   // local storage. HINT: What does `this` reference in the click listener
   // function? How can DOM traversal be used to get the "hour-x" id of the
@@ -23,14 +33,13 @@ $(function () {
   // past, present, and future classes? How can Day.js be used to get the
   // current hour in 24-hour time?
   
-  // current hour variable
-  var currentHour = dayjs().hour();
+  
 
-  // If statements for 9AM
+
   console.log(currentHour);
   if (9 < currentHour) {
     $("#hour-9").addClass("past");
-  }
+  } 
 
   if (9 === currentHour) {
     $("#hour-9").addClass("present");
@@ -47,6 +56,5 @@ $(function () {
   // the values of the corresponding textarea elements. HINT: How can the id
   // attribute of each time-block be used to do this?
   //
-  // TODO: Add code to display the current date in the header of the page.
   
 });
